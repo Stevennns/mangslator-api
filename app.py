7a25f21bd8d493540cfdac13b735b2b82821d8f0
Stevens Egli <81189568+Stevennns@users.noreply.github.com>
2023-12-19 16:14:54 -0300

    Added files

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask
 import os
 
 # UPLOAD_FOLDER = "/home/grupo3/mangslator-uploads/"
-UPLOAD_FOLDER = "../mangslator-results/"
+UPLOAD_FOLDER = "/home/grupo3/app/mangslator-results/"
+CONF_RESULTS = "/home/grupo3/mangslator-conf/"
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
